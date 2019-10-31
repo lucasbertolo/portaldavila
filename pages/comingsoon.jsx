21df13e587332f602deaa5f554679b07e1be0c42
Lucas <lucas.bertolo@gatec.com.br>
2019-10-31 18:02:28 -0300

    adding constructor page

---
+++ pages/comingsoon.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../src/components/Header/Header';
+import Construction from '../src/components/Common/Construction';
+
+
+const ComingSoon = () => (
+  <div>
+    <Header />
+    <Construction />
+  </div>
+);
+
+export default ComingSoon;
