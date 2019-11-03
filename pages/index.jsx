a1033b1556f372d6d7986554903d8d95d837c042
Lucas <lucasbertolo2@gmail.com>
2019-11-03 09:50:02 -0300

    adding style to index page

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,15 +7,18 @@ class Index extends React.PureComponent {
   render() {
     return (
       <div className="idx">
-
-        <div className="idx-logo-container">
+        <img
+          src="https://images.unsplash.com/photo-1546292993-2b8f80c7f35b?ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80"
+          alt=""
+          className="idx-image"
+        />
+        <div className="idx-text">
           <span className="idx-logo" />
-        </div>
-        <h1>Remax/Portal</h1>
+          Remax/Portal
 
-
-        <div className="arr-down">
-          <FontAwesomeIcon icon={faAngleDown} />
+          <div className="arr-down">
+            <FontAwesomeIcon icon={faAngleDown} />
+          </div>
         </div>
       </div>
     );
