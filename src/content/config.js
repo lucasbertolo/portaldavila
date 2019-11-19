2417de9d3c92aaa89dac0d079a124eba3cf366dd
Lucas <lucasbertolo2@gmail.com>
2019-11-18 21:41:13 -0300

    adding prod url

--- src/content/config.js
+++ src/content/config.js
@@ -1,6 +1,6 @@
 const config = {
   urlDev: 'http://localhost:8000',
-  urlProd: '',
+  urlProd: 'https://murmuring-reef-56286.herokuapp.com/',
   cdn: 'https://ik.imagekit.io/2agnc6wu5cbty/',
 };
 
