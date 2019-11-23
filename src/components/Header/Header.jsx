fa4de8801a00f96cac2a29046e85c6dda9e6c1ab
Lucas <lucasbertolo2@gmail.com>
2019-11-23 11:40:30 -0300

    closing menu after logout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -39,13 +39,15 @@ export default class Header extends React.Component {
     return handleRegister(user)
       .then((res) => {
         if (res.success) {
-          this.setState({ open: false });
+          this.setState({
+            open: false,
+          });
           return res;
         }
         return res;
       })
       .catch((res) => res);
-  }
+  };
 
   login = (user) => {
     const { handleLogin } = this.props;
@@ -53,12 +55,23 @@ export default class Header extends React.Component {
     return handleLogin(user)
       .then((res) => {
         if (res.success) {
-          this.setState({ open: false });
+          this.setState({
+            open: false,
+          });
           return res;
         }
         return res;
       })
       .catch((res) => res);
+  };
+
+  signOff = () => {
+    const { logOut } = this.props;
+    this.setState({
+      anchorEl: null,
+    });
+
+    logOut();
   }
 
   closeModalLogin = () => {
@@ -80,74 +93,68 @@ export default class Header extends React.Component {
 
   render() {
     const { user, anchorEl, open } = this.state;
-    const {
-      logOut, nonVisibleHeader, isLogged,
-    } = this.props;
-    return (
-      !nonVisibleHeader ? (
-        <header id="header">
-          <nav className="header-wrapper">
-            <span className="header-logo">
-              <span className="logo" />
-              <h2>Remax/Portal</h2>
-            </span>
-            <span className="header-social">
-              <ul>
-                <li className="menu-bars">
-                  <a
-                    className="btn bars"
-                    onClick={this.openMenu}
-                    role="presentation"
-                  >
-                    <i title="menu">
-                      <FontAwesomeIcon icon={faBars} />
-                    </i>
-                  </a>
-                </li>
-                <li>
-                  <div className="header-login-container">
-                    {
-                      isLogged ? (
-                        <div id="avatar-container">
-                          <span
-                            role="presentation"
-                            className="avatarBtn smGlobalBtn"
-                            onClick={this.openDropdown}
-                          >
-                            {user.username}
-                          </span>
-                        </div>
-                      ) : (
-                        <div
-                          className="login"
-                          onClick={this.openModalLogin}
-                          role="presentation"
-                        >
-                        Login
-                        </div>
-                      )
-                    }
-                  </div>
-                </li>
-              </ul>
-            </span>
-          </nav>
-          <Menu />
-          <AvatarDropdown
-            logOut={logOut}
-            anchorEl={anchorEl}
-            handleClick={this.openDropdown}
-            handleClose={this.closeDropdown}
-          />
-          <ModalLogin
-            open={open}
-            classes="login-modal"
-            handleLogin={this.login}
-            handleRegister={this.register}
-            handleClose={this.closeModalLogin}
-          />
-        </header>
-      ) : null
-    );
+    const { nonVisibleHeader, isLogged } = this.props;
+    return !nonVisibleHeader ? (
+      <header id="header">
+        <nav className="header-wrapper">
+          <span className="header-logo">
+            <span className="logo" />
+            <h2>Remax/Portal</h2>
+          </span>
+          <span className="header-social">
+            <ul>
+              <li className="menu-bars">
+                <a
+                  className="btn bars"
+                  onClick={this.openMenu}
+                  role="presentation"
+                >
+                  <i title="menu">
+                    <FontAwesomeIcon icon={faBars} />
+                  </i>
+                </a>
+              </li>
+              <li>
+                <div className="header-login-container">
+                  {isLogged ? (
+                    <div id="avatar-container">
+                      <span
+                        role="presentation"
+                        className="avatarBtn smGlobalBtn"
+                        onClick={this.openDropdown}
+                      >
+                        {user.username}
+                      </span>
+                    </div>
+                  ) : (
+                    <div
+                      className="login"
+                      onClick={this.openModalLogin}
+                      role="presentation"
+                    >
+                      Login
+                    </div>
+                  )}
+                </div>
+              </li>
+            </ul>
+          </span>
+        </nav>
+        <Menu />
+        <AvatarDropdown
+          logOut={this.signOff}
+          anchorEl={anchorEl}
+          handleClick={this.openDropdown}
+          handleClose={this.closeDropdown}
+        />
+        <ModalLogin
+          open={open}
+          classes="login-modal"
+          handleLogin={this.login}
+          handleRegister={this.register}
+          handleClose={this.closeModalLogin}
+        />
+      </header>
+    ) : null;
   }
 }
