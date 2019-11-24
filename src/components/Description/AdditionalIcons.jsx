91d7579d6c9838348faeba87d672f2c596fdc282
Lucas <lucasbertolo2@gmail.com>
2019-11-24 13:10:06 -0300

    adding label to house description

--- src/components/Description/AdditionalIcons.jsx
+++ src/components/Description/AdditionalIcons.jsx
@@ -25,106 +25,109 @@ const AdditionalIcons = ({ details }) => {
   } = details;
 
   return (
-    <div className="additional-icons">
-      {dormitory === 1 && (
-        <div className="icon-box">
-          <div className="add-feature">
-            <span className="dormitory-icon">
-              <BedIcon />
-            </span>
+    <div className="additional-icons-box">
+      <h3>Outros cômodos: </h3>
+      <div className="additional-icons">
+        {dormitory === 1 && (
+          <div className="icon-box">
+            <div className="add-feature">
+              <span className="dormitory-icon">
+                <BedIcon />
+              </span>
+            </div>
+            <p>Dormitório</p>
           </div>
-          <p>Dormitório</p>
-        </div>
-      )}
-      {garage === 1 && (
-        <div className="icon-box">
-          <div className="add-feature">
-            <span className="garage-icon">
-              <GarageIcon />
-            </span>
+        )}
+        {garage === 1 && (
+          <div className="icon-box">
+            <div className="add-feature">
+              <span className="garage-icon">
+                <GarageIcon />
+              </span>
+            </div>
+            <p>Garagem</p>
           </div>
-          <p>Garagem</p>
-        </div>
-      )}
+        )}
 
-      {bathroom === 1 && (
-        <div className="icon-box">
-          <div className="add-feature">
-            <span className="bathroom-icon">
-              <BathIcon />
-            </span>
+        {bathroom === 1 && (
+          <div className="icon-box">
+            <div className="add-feature">
+              <span className="bathroom-icon">
+                <BathIcon />
+              </span>
+            </div>
+            <p>Banheiro</p>
           </div>
-          <p>Banheiro</p>
-        </div>
-      )}
-      {kitchen === 1 && (
-        <div className="icon-box">
-          <div className="add-feature">
-            <span className="kitchen-icon">
-              <KitchenIcon />
-            </span>
+        )}
+        {kitchen === 1 && (
+          <div className="icon-box">
+            <div className="add-feature">
+              <span className="kitchen-icon">
+                <KitchenIcon />
+              </span>
+            </div>
+
+            <p>Cozinha</p>
           </div>
+        )}
 
-          <p>Cozinha</p>
-        </div>
-      )}
+        {dining_room === 1 && (
+          <div className="icon-box">
+            <div className="add-feature">
+              <span className="box-icon dining-room-icon">
+                <DiningRoomIcon />
+              </span>
+            </div>
 
-      {dining_room === 1 && (
-        <div className="icon-box">
-          <div className="add-feature">
-            <span className="box-icon dining-room-icon">
-              <DiningRoomIcon />
-            </span>
+            <p>Sala de Jantar</p>
           </div>
+        )}
+        {laundry === 1 && (
+          <div className="icon-box">
+            <div className="add-feature">
+              <span className="box-icon laundry-icon">
+                <LaundryIcon />
+              </span>
+            </div>
 
-          <p>Sala de Jantar</p>
-        </div>
-      )}
-      {laundry === 1 && (
-        <div className="icon-box">
-          <div className="add-feature">
-            <span className="box-icon laundry-icon">
-              <LaundryIcon />
-            </span>
+            <p>Lavanderia</p>
           </div>
+        )}
+        {office === 1 && (
+          <div className="icon-box">
+            <div className="add-feature">
+              <span className="box-icon office-icon">
+                <OfficeIcon />
+              </span>
+            </div>
 
-          <p>Lavanderia</p>
-        </div>
-      )}
-      {office === 1 && (
-        <div className="icon-box">
-          <div className="add-feature">
-            <span className="box-icon office-icon">
-              <OfficeIcon />
-            </span>
+            <p>Escritório</p>
           </div>
+        )}
 
-          <p>Escritório</p>
-        </div>
-      )}
+        {living_room === 1 && (
+          <div className="icon-box">
+            <div className="add-feature">
+              <span className="box-icon living-room-icon">
+                <LivingRoomIcon />
+              </span>
+            </div>
 
-      {living_room === 1 && (
-        <div className="icon-box">
-          <div className="add-feature">
-            <span className="box-icon living-room-icon">
-              <LivingRoomIcon />
-            </span>
+            <p>Sala de Estar</p>
           </div>
+        )}
+        {washbasin === 1 && (
+          <div className="icon-box">
+            <div className="add-feature">
+              <span className="box-icon washbasin-icon">
+                <WashbasinIcon />
+              </span>
+            </div>
 
-          <p>Sala de Estar</p>
-        </div>
-      )}
-      {washbasin === 1 && (
-        <div className="icon-box">
-          <div className="add-feature">
-            <span className="box-icon washbasin-icon">
-              <WashbasinIcon />
-            </span>
+            <p>Lavabo</p>
           </div>
-
-          <p>Lavabo</p>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 };
