3dd908ba8ffb41e1584d0285bd091d65d4ebc9b8
Lucas <lucasbertolo2@gmail.com>
2019-11-24 22:38:56 -0300

    stylizing modal box

--- src/components/Contact/EmailForm.jsx
+++ src/components/Contact/EmailForm.jsx
@@ -1,34 +1,11 @@
 import React, { useState } from 'react';
 
-import { withStyles } from '@material-ui/core/styles';
 import { Formik, ErrorMessage } from 'formik';
 import TextField from '@material-ui/core/TextField';
 import { ValidationEmail } from '../Helpers/Validation';
 import db from '../Helpers/ApiFetch';
 
 import './EmailForm.scss';
-
-const CssTextField = withStyles({
-  root: {
-    '& label.Mui-focused': {
-      color: '#fff',
-    },
-    '& .MuiInput-underline:after': {
-      borderBottomColor: '#fff',
-    },
-    '& .MuiOutlinedInput-root': {
-      '& fieldset': {
-        borderColor: 'rgba(255,255,255,0.7)',
-      },
-      '&:hover fieldset': {
-        borderColor: '#66fcf1',
-      },
-      '&.Mui-focused fieldset': {
-        borderColor: '#66fcf1',
-      },
-    },
-  },
-})(TextField);
 
 const EmailForm = () => {
   const [sendStatus, setSendStatus] = useState('');
@@ -65,14 +42,18 @@ const EmailForm = () => {
     >
       {(formikProps) => {
         const {
-          values, handleChange, handleBlur, handleSubmit, errors,
+          values,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          errors,
         } = formikProps;
 
         return (
           <form noValidate onSubmit={handleSubmit} className="email-form">
             <h2>Envie sua mensagem</h2>
             <div>
-              <CssTextField
+              <TextField
                 type="text"
                 name="name"
                 value={values.name}
@@ -80,26 +61,26 @@ const EmailForm = () => {
                 onBlur={handleBlur}
                 className="input"
                 label="Nome"
+                variant="outlined"
                 error={!!errors.name}
               />
 
-              <CssTextField
+              <TextField
                 type="text"
                 name="phone"
                 value={values.phone}
                 onChange={handleChange}
                 onBlur={handleBlur}
-                // error={!!errors.registerEmail}
+                variant="outlined"
                 className="input"
                 label="Celular"
               />
               <div>
-                <ErrorMessage component="span" name="phone" />
                 <ErrorMessage component="span" name="name" />
               </div>
             </div>
             <div>
-              <CssTextField
+              <TextField
                 fullWidth
                 type="email"
                 name="email"
@@ -109,6 +90,7 @@ const EmailForm = () => {
                 // error={!!errors.registerEmail}
                 className="input"
                 label="Email"
+                variant="outlined"
                 error={!!errors.email}
               />
               <div>
@@ -116,7 +98,7 @@ const EmailForm = () => {
               </div>
             </div>
             <div>
-              <CssTextField
+              <TextField
                 type="text"
                 name="message"
                 fullWidth
@@ -128,6 +110,7 @@ const EmailForm = () => {
                 onBlur={handleBlur}
                 className="input"
                 label="Mensagem"
+                variant="outlined"
                 error={!!errors.message}
               />
               <div>
