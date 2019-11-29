f8bd32e69653a449e93f2107739aa6f53ec923df
Lucas <lucas.bertolo@gatec.com.br>
2019-11-29 12:03:46 -0300

    resizing images

--- src/components/Description/SliderImages.jsx
+++ src/components/Description/SliderImages.jsx
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const SliderImages = ({ images }) => {
+  const [coverSize, setCoverSize] = useState(700);
+  const [leftAsideSize, setLeftAsideSize] = useState(400);
+  const [rightAsideSize, setRightAsideSize] = useState(400);
+  useEffect(() => {
+    if (process.browser) {
+      const item = document.querySelector('#cover');
+      setCoverSize(item.width);
+
+      const left = document.querySelector('#left-aside');
+      const right = document.querySelector('#right-aside');
+
+      if (left) setLeftAsideSize(left.width);
+      if (right) setRightAsideSize(right.width);
+    }
+  }, []);
+
   const defineSize = (mode, src) => {
     switch (mode) {
       case 'cover':
-        return `${src}?tr=w-550`;
-      case 'aside':
-        return `${src}?tr=w-400`;
+        return `${src}?tr=w-${coverSize}`;
+      case 'left-aside':
+        return `${src}?tr=w-${leftAsideSize}`;
+      case 'right-aside':
+        return `${src}?tr=w-${rightAsideSize}`;
       default:
         return src;
     }
   };
   const cover = images.filter((item) => item.iscover).map((x) => (
     <figure key={x.url} className="shine">
-      <img src={defineSize('cover', x.cdn) || x.url} alt={x.alt} />
+      <img src={defineSize('cover', x.cdn) || x.url} alt={x.alt} id="cover" />
     </figure>
   ));
 
@@ -27,13 +45,12 @@ const SliderImages = ({ images }) => {
     if (slideImages.length >= 1 && slideImages.length < 3) {
       const slide = slideImages.map((x) => (
         <figure key={x.url} className="shine">
-          <img src={defineSize('aside', x.cdn) || x.url} alt={x.alt} />
+          <img src={defineSize('left-aside', x.cdn) || x.url} alt={x.alt} id="left-aside" />
         </figure>
       ));
 
       return (
         <div className="slider-photos">
-
           <div className="slider-left">
             {slide}
           </div>
@@ -44,19 +61,18 @@ const SliderImages = ({ images }) => {
     if (slideImages.length >= 3) {
       const leftSlider = slideImages.slice(0, 2).map((x) => (
         <figure key={x.url} className="shine">
-          <img src={defineSize('aside', x.cdn) || x.url} alt={x.alt} />
+          <img src={defineSize('left-aside', x.cdn) || x.url} alt={x.alt} id="left-aside" />
         </figure>
       ));
 
       const rightslider = slideImages.slice(2, 4).map((x) => (
         <figure key={x.url} className="shine">
-          <img src={defineSize('aside', x.cdn) || x.url} alt={x.alt} />
+          <img src={defineSize('right-aside', x.cdn) || x.url} alt={x.alt} id="right-aside" />
         </figure>
       ));
 
       return (
         <div className="slider-photos">
-
           <div className="slider-left">
             {leftSlider}
           </div>
